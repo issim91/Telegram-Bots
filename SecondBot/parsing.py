dbe138b08af381b69673d1ecf76465485664ccd5
Илья Симонов <ilasimonov@MacBook-Air-Ila.local>
2018-11-27 16:26:11 +0400

    Add function round for rounding to 2 decimal places

--- SecondBot/parsing.py
+++ SecondBot/parsing.py
@@ -13,12 +13,11 @@ def parse_text(text):
 def get_price(crypto):
     crypto = parse_text(crypto)
     crypto = crypto.upper()
-    print(crypto)
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol={}'.format(crypto)
-    print(url)
     try:
         r = requests.get(url, headers=misc.headers).json()
         price = r['data'][crypto]['quote']['USD']['price']
+        price = round(price, 2)
         return 'Курс ' + crypto + ' = ' + str(price) + ' $'
     except:
         return "Такой криптовалюты нет. Введите символ криптовалюты"
